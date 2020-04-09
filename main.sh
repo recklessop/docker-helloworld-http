d6ba0ce53608a66dc687492a6b9a7196dcd92412
Justin Paul <justin@jpaul.me>
2020-04-09 19:23:46 -0400

    Update main.sh
    converting to python 3.8

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 echo '<html><head><title>HTTP Hello World</title></head><body><h1>Hello from '$(hostname)'</h1></body></html' > /www/index.html
 
-python -m SimpleHTTPServer 80
+python -m http.server 80
